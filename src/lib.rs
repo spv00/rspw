27094a8d90fee6ab53dc8d6c1fba33f06ac8ee09
Sean Voss <seany@localhost.localdomain>
2022-07-25 23:53:45 +0200

    bruh

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use std::fs;
+
+use colored::Colorize;
 use rand::prelude::SliceRandom;
 use rand;
+use libm;
 #[allow(dead_code, unused_imports)]
 
 const DIGITS: [&'static str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -7,16 +11,17 @@ const LOWERCASE: [&'static str; 26] = ["a", "b", "c", "d", "e", "f", "g", "h", "
 const UPPERCASE: [&'static str; 26] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const SPECIAL: [&'static str; 9] = ["!", "$", "%", "(", ")", "/", "#", "+", "?"];
 
-pub fn generate_pass<'a>(len: i32, config: &'a Config) -> String{
+pub fn generate_pass<'a>(config: &'a Config) -> String{
     
     /*let len = rand::thread_rng().gen_range(0..len)
         .try_into()
         .unwrap_or_else(|_| {5});*/
+    //Choose multiple chars from the provided array in the config using choose_multiple from SliceRandom
     let out: Vec<&'static str>  = config.chars.choose_multiple(
         &mut rand::thread_rng(),
-        len.try_into().unwrap_or_else(|_| {
+        config.len.try_into().unwrap_or_else(|_| {
             // Handle Error case if no valid USIZE is provided for password length
-            eprintln!("No valid length provided. Expected usize but got {}. Defaulting to 16", len);
+            eprintln!("No valid length provided. Expected usize but got {}. Defaulting to 16", config.len);
             16
         }))
         .map(|x| {x.to_owned()})
@@ -24,6 +29,36 @@ pub fn generate_pass<'a>(len: i32, config: &'a Config) -> String{
     out.join("")
 }
 
+pub fn format_pass(pass: String, entropy: f64) -> String{
+    //Format the output using the "format" file
+    /*
+        <pasword> is replaced by the actual password
+    */
+
+    let format = fs::read_to_string("format").unwrap_or_else(|_| {"
+    =====
+    <password>
+
+    Entropy: <entropy>
+    =====
+    ".to_string()});
+    let format = format.replace("<password>", {
+        if entropy < 32_f64{pass.as_str()}
+        else{pass.as_str()}
+    }).replace("<entropy>", entropy.to_string().as_str());
+    format
+}
+
+pub fn calc_entropy<'a>(len: i32, pool_size: i32) -> f64{
+    //Calculate the passwords entropy (log(pow(poolSize, length), 2))
+    libm::log2(libm::pow(pool_size.into(), len.into()).into()).round()
+}
+
+pub fn generate_final<>() -> String{
+    let config: Config = Config::new(5, true, true, true, false);
+    let entropy = calc_entropy(config.len, config.chars.len() as i32);
+    format_pass(generate_pass(&config), entropy)
+}
 
 #[derive(Clone)]
 pub struct Config{
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod lib;
 fn main() {
-    let config: lib::Config = lib::Config::new(5, true, true, true, false);
-    println!("{}", lib::generate_pass(5, &config));
+    println!("{}", lib::generate_final());
 }
